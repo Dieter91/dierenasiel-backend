86f9cc422d9e2068fa61ce3a5419be1416f98844
Dieter Vrancken <dieter.v91@gmail.com>
2022-05-05 09:45:37 +0200

    nieuwe post aanmaken met img upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|alpha_dash|unique:posts',
+            'content' => 'required',
+            'img' => 'required|image'
+        ]);
+
+
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->slug = $request->slug;
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->img = $request->file('img')->store('posts', 'public');
+        $result = $post->save();
+
+        if ($result) {
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json(['success' => false]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
